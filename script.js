d1996b8f253aa03e8a26cd393a07194b9ae1d6de
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2023-12-21 01:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1.charAt(0)!=="" && s2.charAt(0)!=="")
+		return s1.charAt(0)+s2.charAt(0);
+		return "";	
 }
 
 // Do not change the code below.
